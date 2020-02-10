e305cf5473e0d2ed7f66128417685d607dcc6888
Edvard Heikel <edvardheikel@gmail.com>
2020-02-10 07:07:10 +0200

    detail view

--- routes/shop.js
+++ routes/shop.js
@@ -12,8 +12,9 @@ const router = express.Router();
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
+router.get('/product-detail/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
-router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
