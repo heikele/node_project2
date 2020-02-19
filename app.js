8e51258fcb01ce6c5a7bfe9a3ffd2af719817406
Edvard Heikel <edvardheikel@gmail.com>
2020-02-19 09:37:04 +0200

    add cart to db

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -36,9 +38,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User); // not necessary
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  //.sync({ force: true }) only for forcing complete reinstantiation of database
+  // .sync({ force: true }) //only for forcing complete reinstantiation of database
   .sync()
   .then(result => {
     return User.findByPk(1);
@@ -54,6 +60,9 @@ sequelize
   })
   .then(user => {
     //console.log(user);
+    user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
